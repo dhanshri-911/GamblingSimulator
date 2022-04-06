23d37597a933eb963f4c658bcdc3c9a94318f2e1
Dhanshri911 <dszingade911@gmail.com>
2022-04-06 19:41:15 +0530

    Welcome to Gambling Game

---
+++ src/GamblingSimulator.java
@@ -0,0 +1,10 @@
+public class GamblingSimulator {
+    public static final int STACK_PER_DAY = 100;
+    public static final int BET_PER_GAME = 1;
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Game..");
+
+    }
+}
