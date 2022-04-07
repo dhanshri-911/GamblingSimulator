ac6ab9d1b174e313ff2e2494fd63f40bc357b02d
Dhanshri911 <dszingade911@gmail.com>
2022-04-07 17:41:38 +0530

    UC2 :Added winning or loosing

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class GamblingSimulator {
-    public static final int STACK_PER_DAY = 100;
-    public static final int BET_PER_GAME = 1;
-
-
+    static int AMOUNT_OF_STAKE=100;
+    static int BET_AMOUNT=1;
+    static int WON=1;
+    static int LOST=0;
     public static void main(String[] args) {
-        System.out.println("Welcome to Gambling Game..");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting amount of stake : "+AMOUNT_OF_STAKE);
+        System.out.println("Amount to bet after every play : "+BET_AMOUNT);
+        int play = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println("Gamblers play:"+play);
+        int InHandcash = AMOUNT_OF_STAKE;
+        switch(play) {
+            case 1:
+                InHandcash = InHandcash + BET_AMOUNT;
+                System.out.println("Total current cash gambler has "+InHandcash);
+                break;
+            case 2:
+                InHandcash = InHandcash - BET_AMOUNT;
+                System.out.println("Total current cash gambler has "+InHandcash);
+                break;
+            default:
+                System.out.println("default");
+        }
+        if(play == 1) {
+            System.out.println("Player had won the first play");
+        }
+        else {
+            System.out.println("Player had lost the first play");
+        }
 
     }
+
 }
